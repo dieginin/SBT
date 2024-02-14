65ffa689ee91125cb9fd3b73fb7685de207e4eb8
dieginin <diegobalestra9@gmail.com>
2024-02-13 19:34:59 -0500

    router added

---
+++ core/router.py
@@ -0,0 +1,45 @@
+from flet import Page, RouteChangeEvent, Row, VerticalDivider
+
+from components import NavRail
+from databases import Location, Staff
+from views import *
+
+app = Row(expand=True)
+routes = {
+    "/": Dashboard,
+    "/dsr": DSR,
+    "/noconfig": NoConfig,
+    "/openclose": OpenClose,
+    "/schedule": Schedule,
+    "/setschedule": SetSchedule,
+    "/settings": Settings,
+}
+
+
+class Router:
+    def __init__(self, page: Page):
+        self.page = page
+        self.page.on_route_change = self.route_change
+        self.rail = NavRail(self.page)
+        app.controls = [self.rail, VerticalDivider(width=1), routes["/"](self.page)]
+
+        self.initial_route()
+        self.page.add(app)
+
+    @property
+    def config(self) -> bool:
+        return Location().name and Staff().members
+
+    def initial_route(self):
+        if not self.config:
+            self.rail.selected_index = None
+            app.controls[2] = routes["/noconfig"](self.page)
+
+    def route_change(self, event: RouteChangeEvent):
+        if not self.config and event.route != "/settings":
+            self.rail.selected_index = None
+            r = "/noconfig"
+        else:
+            r = event.route
+        app.controls[2] = routes[r](self.page)
+        self.page.update()
