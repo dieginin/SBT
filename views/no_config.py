f45f84ea55a8bf7544069f9703555c4b35d0f7aa
dieginin <diegobalestra9@gmail.com>
2024-02-14 00:33:30 -0500

    no config view finished

--- views/no_config.py
+++ views/no_config.py
@@ -1,12 +1,17 @@
 from flet import Container, Page
 
 from components import Alert
+from databases import Location, Staff
 
 
 class NoConfig(Container):
     def __init__(self, page: Page):
         super().__init__()
+        location = not Location().name
+        staff = not Staff().members
+        
+        string = f"{"store" if location else ""}{" and " if location & staff else ""}{"staff" if staff else ""}"
         self.content = Alert(
             "manage_accounts_rounded",
-            "First setup store and/or staff\nPlease go to settings",
+            f"First setup {string}\nPlease go to settings",
         )
